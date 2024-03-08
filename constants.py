151c7ff138383c67ae84093443c3e7c09380def2
Benoît Prieur <benoitprieur@benoits-macbook-pro.home>
2024-03-08 09:22:53 +0100

    a lot, wikidata integration, redirs mgt

--- constants.py
+++
@@ -1,7 +0,0 @@
-DB_NAME = 'StatsWiki00.db'
-CURRENT_YEAR = 2024
-SUPPORTED_YEARS = [2015, 2016, 2017, 2018, 2019, 2020, 2021, 2022, 2023, 2024]
-SUPPORTED_LANGUAGES = ['ar', 'de', 'en', 'eo', 'es', 'fr', 'ja', 'he', 'hy', 'it', 'ko', 'nl', 'pl', 'pt', 'ru', 'uk', 'zh']
-SQL_LIMIT = 100
-
-
